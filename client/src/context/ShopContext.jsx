55d3b9020f380cc9efbcb46951a1bda85dcff137
payal105 <payaladhikary2000@gmail.com>
2025-06-16 23:49:30 +0530

    Latest Collection

--- client/src/context/ShopContext.jsx
+++ client/src/context/ShopContext.jsx
@@ -1,14 +1,21 @@
-const { createContext } = require("react");
+import React, { createContext, useState } from 'react';
+// This file creates a context for the shop, providing products, currency, and delivery fee.
+import {products} from '../assets/assets'
 
 export const ShopContext = createContext();
 
-const ShopContextProvider = () =>{
-    const value = {
+const ShopContextProvider = (props) =>{
+    const [currency, setCurrency] = useState('$');
+    const [deliveryFee, setDeliveryFee] = useState(10);
 
+    const value = {
+        products, currency, deliveryFee, setCurrency, setDeliveryFee
     }
     return (
-        <ShopContext.Provider  value={value}>
+        <ShopContext.Provider value={value}>
             {props.children}
         </ShopContext.Provider>
     )
 }
+
+export default ShopContextProvider;
