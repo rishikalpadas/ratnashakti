58bdd71e9f670fb6fa07064a3dc5fd4dbbeb76cc
payal105 <payaladhikary2000@gmail.com>
2025-07-02 23:04:53 +0530

    Till Login UI

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -5,7 +5,7 @@ import { ShopContext } from '../context/ShopContext'
 
 const Navbar = () => {
     const [visible, setVisible] = useState(false);
-    const {setShowSearch,getCartCount} = useContext(ShopContext);
+    const { setShowSearch, getCartCount } = useContext(ShopContext);
     return (
         <div className='flex items-center justify-between py-5 font-medium'>
 
@@ -39,7 +39,9 @@ const Navbar = () => {
                 <img onClick={() => setShowSearch(true)} src={assets.search_icon} className='w-5 cursor-pointer' alt="Search Icon" />
 
                 <div className='group relative'>
-                    <img src={assets.profile_icon} className='w-5 cursor-pointer' alt="Profile Icon" />
+                    <Link to='/login'>
+                        <img src={assets.profile_icon} className='w-5 cursor-pointer' alt="Profile Icon" />
+                    </Link>
                     <div className='group-hover:block hidden absolute dropdown-menu right-0 pt-4'>
                         <div className='flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 text-gray-500 rounded'>
                             <p className='cursor-pointer hover:text-black'>My Profile</p>
@@ -50,22 +52,22 @@ const Navbar = () => {
                 </div>
                 <Link to='/cart' className='relative'>
                     <img src={assets.cart_icon} className='w-5 min-w-5' alt="Cart Icon" />
-                    <p className='absolute right-[-5px] bottom-[-5px] w-4 text-center leading-4 bg-black text-white aspect-square rounded-full text-[8px]'>{getCartCount()>0 ? getCartCount() : 0}</p>
+                    <p className='absolute right-[-5px] bottom-[-5px] w-4 text-center leading-4 bg-black text-white aspect-square rounded-full text-[8px]'>{getCartCount() > 0 ? getCartCount() : 0}</p>
                 </Link>
 
                 <img onClick={() => setVisible(true)} src={assets.menu_icon} className='w-5 cursor-pointer sm:hidden' alt="Menu Icon" />
             </div>
             {/* Sidebar Menu for small screens */}
-            <div className={`absolute top-0 right-0 bottom-0 overflow-hidden bg-white transition-all ${visible? 'w-full':'w-0'}`}>
+            <div className={`absolute top-0 right-0 bottom-0 overflow-hidden bg-white transition-all ${visible ? 'w-full' : 'w-0'}`}>
                 <div className='flex flex-col text-gray-600'>
-                        <div onClick={()=>setVisible(false)} className='flex items-center gap-4 p-3'>
-                            <img src={assets.dropdown_icon} className='h-4 rotate-180' alt=''/>
-                            <p>Back</p>
-                        </div>
-                        <NavLink onClick={()=>setVisible(false)} to='/' className='py-2 pl-6 border'>HOME</NavLink>
-                        <NavLink onClick={()=>setVisible(false)} to='/collection' className='py-2 pl-6 border'>COLLECTION</NavLink>
-                        <NavLink onClick={()=>setVisible(false)} to='/about' className='py-2 pl-6 border'>ABOUT</NavLink>
-                        <NavLink onClick={()=>setVisible(false)} to='/contact' className='py-2 pl-6 border'>CONTACT</NavLink>
+                    <div onClick={() => setVisible(false)} className='flex items-center gap-4 p-3'>
+                        <img src={assets.dropdown_icon} className='h-4 rotate-180' alt='' />
+                        <p>Back</p>
+                    </div>
+                    <NavLink onClick={() => setVisible(false)} to='/' className='py-2 pl-6 border'>HOME</NavLink>
+                    <NavLink onClick={() => setVisible(false)} to='/collection' className='py-2 pl-6 border'>COLLECTION</NavLink>
+                    <NavLink onClick={() => setVisible(false)} to='/about' className='py-2 pl-6 border'>ABOUT</NavLink>
+                    <NavLink onClick={() => setVisible(false)} to='/contact' className='py-2 pl-6 border'>CONTACT</NavLink>
                 </div>
             </div>
         </div>
