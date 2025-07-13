2d78f0c8c0d7b88e3a4f3d13d0d968c06d932719
payal105 <payaladhikary2000@gmail.com>
2025-07-13 09:39:34 +0530

    Upto contact page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,9 +1,43 @@
 import React from 'react'
+import Title from '../components/Title'
+import { assets } from '../assets/assets';
+import NewsletterBox from '../components/NewsletterBox'
 
 const About = () => {
   return (
     <div>
-      
+      <div className='text-2xl text-center pt-8 border-t'>
+        <Title text1={'ABOUT'} text2={'US'} />
+      </div>
+      <div className='my-10 flex flex-col md:flex-row gap-16'>
+          <img className='w-full md:max-w-[450px]' src={assets.about_img} alt="" />
+          <div className='flex flex-col justify-center gap-6 md:w-2/4 text-gray-600'>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt earum eius, magnam ipsam temporibus corporis reiciendis, similique minima magni autem molestias facilis totam necessitatibus dolorum? Modi sit neque et quasi.</p>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quidem earum nobis id libero at molestiae nulla assumenda impedit illum minima?</p>
+            <b className='text-gray-800'>Our Mission</b>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quod, reiciendis.</p>
+          </div>
+      </div>
+      <div className='text-xl py-4'>
+        <Title text1={'WHY'} text2={'CHOOSE US?'} />
+      </div>
+
+      <div className='flex flex-col md:flex-row text-sm mb-20'>
+        <div className='border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5'>
+          <b>Quality Assurance :</b>
+          <p className='text-gray-600'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.</p>
+        </div>
+        <div className='border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5'>
+          <b>Convenience :</b>
+          <p className='text-gray-600'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.</p>
+        </div>
+        <div className='border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5'>
+          <b>Exceptional Customer Service :</b>
+          <p className='text-gray-600'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.</p>
+        </div>
+      </div>
+      <NewsletterBox />
+
     </div>
   )
 }
